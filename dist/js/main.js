f6c1754f8a34881e7d19cdbf5a9fd89ea9817f7b
Nikolay <nk.it.enineer@gmail.com>
2019-03-31 18:36:25 +0300

    Фиксины блоки адаптив, шрифты. Карта подгружатся только ели долистали до блока

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,7 +9,7 @@ $(window).load(function(){
 
 	// Slick Slider plugin   http://kenwheeler.github.io/slick/
 	$(".before-slider").slick({
-		dgaggable: false,
+		draggable: false,
 		dots: true,
 		dotsClass: 'before-slider__dots',
 		prevArrow: $(".arrow-left"),
@@ -42,9 +42,22 @@ $(window).load(function(){
 	});
 
 	// MaskedInput   https://github.com/digitalBush/jquery.maskedinput
-
 	$("input[type='tel']").mask("+7 (999) 999-999-999");
 
+	// Показать(загрузить) карту, только если до неё докрутили.
+	var reviews = $('.reviews');
+	var reviewsTop = reviews.offset().top;
+	// вешаем bind на scroll, предотвращаем многоразовую загрузку карты
+	$(window).bind('scroll',function(){
+		var windowTop = $(this).scrollTop();
+		if(windowTop > reviewsTop){
+			console.log('_докрутили_') // отладка
+			// вставить yandex крту:
+			$('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A14b6e83551c5a476b80caeb25f1f406fc1a56b37b564328403a9001becd0d4ab&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>');
+			//перестаём отслеживать
+			$(window).unbind('scroll'); 
+		}
+	});
 
 
 })
